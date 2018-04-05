6a16a5e777f35c76b84b12d50dce7ec371454a8c
Martin Singh <h15mavir@du.se>
2018-04-05 22:46:37 +0200

    Add friendship model

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,14 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   devise_for :users, :controllers => { :registrations => "user/registrations" }
+  resources :user_stocks, only: [:create, :destroy]
 
   root 'welcome#index'
 
   # Här lagras en användares aktier.
   get 'my_portfolio', to: 'users#my_portfolio'
   get 'search_stocks', to: 'stocks#search'
-
-  resources :user_stocks, only: [:create, :destroy]
+  
+  get 'my_friends', to: 'users#my_friends'
+  
 end
