5a030f1e7f74dfd3d050e9c5cdf5b00a5d758c67
Martin Singh <h15mavir@du.se>
2018-04-05 21:08:34 +0200

    Add user and stock association plus views

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,9 @@
 class UsersController < ApplicationController
 
     def my_portfolio
-
+        # För authenticering och kunna hantera en användares stocks.
+        @user = current_user
+        @user_stocks = current_user.stocks
     end
 
 end
